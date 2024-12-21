b45da48459cd195bb849e7baa68d3b97965118e3
Yutt3 <bastianme9k@gmail.com>
2024-12-21 11:36:52 +0800

    Create web-crawler-basics.py
    第一个爬虫基础python-code

---
+++ web-crawler-basics.py
@@ -0,0 +1,35 @@
+# 使用import导入requests模块
+import requests
+
+# 从bs4中导入BeautifulSoup模块
+from bs4 import BeautifulSoup
+
+# 将URL地址赋值给变量url
+url = "https://nocturne-spider.baicizhan.com/2020/08/08/%E5%9F%9F%E5%90%8D/"
+
+# 将变量url传入requests.get()，赋值给response
+response = requests.get(url)
+
+# 将服务器响应内容转换为字符串形式，赋值给html
+html = response.text
+
+# 使用BeautifulSoup()读取html，添加lxml解析器，赋值给soup
+soup = BeautifulSoup(html, "lxml")
+
+# 使用find_all()查询soup中h2节点，赋值给content_all
+content_all = soup.find_all(name="h2")
+
+# for循环遍历content_all
+for content in content_all:
+
+    # TODO 使用.string获取节点的内容，赋值给contentString
+    contentString = content.string
+    
+    # TODO 使用print输出contentString
+    print(contentString)
+
+    # TODO 使用.text获取节点的内容，赋值给contentString
+    contentString = content.text
+    
+    # TODO 使用print输出contentString
+    print(contentString)
